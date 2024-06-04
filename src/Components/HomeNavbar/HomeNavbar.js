30b08dea1820e59dae1a29eb50837d55bb54f91a
NAUFAL GHIFARI RAMADHANA <naufalghifari1139@gmail.com>
2024-06-04 14:19:59 +0700

    third commit

--- src/Components/HomeNavbar/HomeNavbar.js
+++ src/Components/HomeNavbar/HomeNavbar.js
@@ -4,8 +4,13 @@ import Vector from '../Assets/Vector.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import Model from "react-modal"
+import { useState } from 'react'
 
 export default function HomeNavbar() {
+
+  const [visible, setvisible]= useState(false)
   return (
     <div>
         <div className="homenav">
@@ -26,8 +31,30 @@ export default function HomeNavbar() {
             <p>English</p>
             </div>
             <div className="nav-login">
-            <button>Sign Up</button>
-            <button className='register'>Join Us</button>
+            <button onClick={()=>setvisible(true)}>Sign Up</button>
+            <Model className="model" isOpen={visible} onRequestClose={()=>setvisible(false)} style={{
+              content:{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                padding: "10px",
+              },
+
+            }}>
+              <button className="close-button"onClick={()=>setvisible(false)}><FontAwesomeIcon icon={faX} /></button>
+              <h1>Sign Up</h1>
+              <p>Don’t have account ? sign in <a className="register-link" href='/#register'>here</a></p>
+
+              <input type="text" className="login-input"placeholder="Email" name="email" required></input><br></br>
+              <br></br>
+              <input type="password" className='login-input' placeholder="Password" name="psw" required></input><br></br>
+
+              <button class="button-13" role="button">Sign In</button>
+            </Model>
+            <a className="register-link" href='/#register'>
+            <button className='register' >Join Us</button>
+            </a>
         </div>
       </div>
     </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,10 +5,15 @@ import hand from '../Assets/hand.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import Model from "react-modal"
+import { useState } from 'react'
 
 
 
 export default function Navbar() {
+
+  const [visible, setvisible]= useState(false)
   return (
     <div className='navbar'>
       <div className="nav">
@@ -18,7 +23,8 @@ export default function Navbar() {
         </div>
         <ul className='nav-menu'>
             <li><a href='/#dashboard'>Home</a></li>
-
+            <li><a href='/#category'>Category</a></li>
+            <li><a href='/#aboutus'>About</a></li>
 
         </ul>
         <div className="nav-world">
@@ -26,8 +32,31 @@ export default function Navbar() {
             <p>English</p>
             </div>
             <div className="nav-login">
-            <button>Sign Up</button>
-            <button className='register'>Join Us</button>
+            <button onClick={()=>setvisible(true)}>Sign Up</button>
+            <Model className="model" isOpen={visible} onRequestClose={()=>setvisible(false)} style={{
+              content:{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                padding: "10px",
+
+              }
+            }}>
+              <button className="close-button"onClick={()=>setvisible(false)}><FontAwesomeIcon icon={faX} /></button>
+              <h1>Sign Up</h1>
+              <p>Don’t have account ? sign in <a className="register-link" href='/#register'>here</a></p>
+
+              <input type="text" className="login-input"placeholder="Email" name="email" required></input><br></br>
+              <br></br>
+              <input type="password" className='login-input' placeholder="Password" name="psw" required></input><br></br>
+
+              <button class="button-13" role="button">Sign In</button>
+            </Model>
+            <a className="register-link" href='/#register'>
+            <button className='register' >Join Us</button>
+            </a>
+            
         </div>
       </div>
 
@@ -39,8 +68,8 @@ export default function Navbar() {
       <div className="jumbotron">
 
    <div class="search">
-      <input type="text" class="searchTerm" placeholder="What are you looking for?"/>
-      <button type="submit" class="searchButton">
+      <input type="text" className="searchTerm" placeholder="What are you looking for?"/>
+      <button type="submit" className="searchButton">
       <FontAwesomeIcon icon={faMagnifyingGlass} />
      </button>
    </div>
